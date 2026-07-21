75bd24a6a438686b2c55dbf89f3553d6d777c444
HARSHITH-28 <harshithc2828@gmail.com>
2026-07-22 00:57:26 +0530

    deployment config 2.0

--- app.py
+++ app.py
@@ -2744,4 +2744,4 @@ def add_admin():
 # Run the app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port)
